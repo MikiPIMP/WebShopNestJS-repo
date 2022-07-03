928a1263dfdba12836bc21be3c5d8a974fe800a6
Miki_806 <milan.mm.minic@gmail.com>
2022-07-03 17:00:40 +0200

    Inicijalni komit - generisanje osnovne strukture aplikacije uz pomoc nest.js

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
+
+//-----------------------------------------
+//Milan: [03.07.2022. u 17h]
+//npm run start:dev
+//aplikacija radi kod mene lokalhostu na portu 3000
+//localhost:3000
+//Aplikacija se iskljucuje sa komandom na tastaturi: ctrl+c
+//                                                   y+enter
+//-----------------------------------------
